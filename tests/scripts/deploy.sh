9a884cdab04981e52ae283b1ee91c705cd15e1b0
Rafal Krysiak <rafal@near-me.com>
2024-05-06 18:01:24 +0200

    Link global playwright to local project after all

--- tests/scripts/deploy.sh
+++ tests/scripts/deploy.sh
@@ -2,8 +2,8 @@ set -e
 DEFAULT_ENV=""
 POS_ENV="${1:-$DEFAULT_ENV}"
 
-#echo "Linking playwright"
-#npm link @playwright/test
+echo "Linking playwright"
+npm link @playwright/test
 
 pos-cli data clean $POS_ENV --auto-confirm --include-schema
 pos-cli deploy $POS_ENV
